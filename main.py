b0f5da39cfe4108c97dda0a2484f7309c69ef918
Vsevolod-Afonin <Vsevladey@yandex.ru>
2025-03-22 16:40:24 +0700

    add all const

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtledemo.nim import SCREENWIDTH, SCREENHEIGHT
 
 import pygame
+import random
 
 pygame.init()
 
@@ -9,7 +10,22 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Игра ТИР')
-icon = pygame.image.load('')
+icon = pygame.image.load('image\icon.png')
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load('image\Мишень.png')
+target_width = 80
+target_heidth = 80
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_heidth)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+
+
+
 
 running = True
 while running:
