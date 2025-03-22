b9d069e4eec74d379816f18e1630a6610debacfc
Vsevolod-Afonin <Vsevladey@yandex.ru>
2025-03-22 16:13:05 +0700

    add new const

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from turtledemo.nim import SCREENWIDTH, SCREENHEIGHT
+
 import pygame
 
 pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption('Игра ТИР')
+icon = pygame.image.load('')
 
 running = True
 while running:
