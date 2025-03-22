4706321488136a255fcf46e4ee0053cebbe920fc
Vsevolod-Afonin <Vsevladey@yandex.ru>
2025-03-22 17:22:28 +0700

    full functional

--- main.py
+++ main.py
@@ -23,12 +23,20 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_heidth)
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
-
-
-
-
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_heidth:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_heidth)
+
+
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
