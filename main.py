a2f193d1b68a4f88f16295275ef10b812d5dcd63
Vsevolod-Afonin <Vsevladey@yandex.ru>
2025-03-27 14:32:38 +0700

    add sound's files

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
 pygame.display.set_caption('Игра ТИР')
 icon = pygame.image.load('image\icon.png')
 pygame.display.set_icon(icon)
