6c89f9356b4bd0dd0fabf96dfee85f08e8c7920d
Vsevolod-Afonin <Vsevladey@yandex.ru>
2025-03-27 15:50:42 +0700

    Добавление счёта и надписи, файл Readme

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
 pygame.display.set_caption('Игра ТИР')
 icon = pygame.image.load('image\icon.png')
 pygame.display.set_icon(icon)
@@ -19,9 +18,15 @@ target_width = 80
 target_heidth = 80
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
-target_y = random.randint(0, SCREEN_HEIGHT - target_heidth)
+target_y = random.randint(50, SCREEN_HEIGHT - target_heidth)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+
+fontik = pygame.font.SysFont('arialblack', 24)
+follow = fontik.render('ПОПАДИ В МИШЕНЬ!', 1, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+k=0
 
 
 running = True
@@ -35,6 +40,15 @@ while running:
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_heidth:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_heidth)
+                k+=1
+            else:
+                if k>5 : k-=5
+                else: k=0
+    cnt = str(k)
+    count = fontik.render(f'Счёт: {cnt}', 1, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    screen.blit(follow, (0, 0))
+    screen.blit(count, (650, 0))
+    pygame.display.update()
 
 
     screen.blit(target_image, (target_x, target_y))
